b05f1c7537adb43cbb1881858a48fc5a2e0a2a6b
Benson <benson.thomas2602@gmail.com>
2023-09-07 23:59:51 -0500

    Fixing the deploy.sh

--- part-2-scripts/deploy.sh
+++ part-2-scripts/deploy.sh
@@ -2,7 +2,7 @@
 export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
 
 cd week-9
- git pull origin master
+ git pull origin main
  cd server
- pm2 kill
- pm2 start index.js
+ npx pm2 kill
+ npx pm2 start index.js
--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,1 @@
-ssh -t -i "todo-app-key.pem" ubuntu@ec2-54-183-219-200.us-west-1.compute.amazonaws.com  "sudo bash ~/deploy.sh"
+ssh -t -i "todo-app-class.pem" ubuntu@ec2-3-101-38-153.us-west-1.compute.amazonaws.com  "sudo bash ~/deploy.sh"
