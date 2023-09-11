5484686998ba053fa4635877c5f3de56b2a4c22a
Benson <benson.thomas2602@gmail.com>
2023-09-11 00:11:55 -0500

    changed export path location -  pm2

--- part-2-scripts/deploy.sh
+++ part-2-scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.6.0/bin
 
 cd week-9
  git pull origin main
  cd server
- npx pm2 kill
- npx pm2 start index.js
+ pm2 kill
+ pm2 start index.js
